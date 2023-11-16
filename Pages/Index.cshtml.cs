ee897a909805e8d8138b037c970d9f5898a469a5
unknown <aopoku255@gmail.com>
2023-11-16 10:07:26 +0000

    first commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace razor.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    [BindProperty(SupportsGet = true)]
+    public string FirstName { get; set; }
+    public void OnGet()
+    {
+        if (string.IsNullOrWhiteSpace(FirstName))
+        {
+            FirstName = "User";
+        }
+    }
+}
